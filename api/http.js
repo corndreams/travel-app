4e93499555e1e5e8c47362fcf14e005159776f1e
corndreams <446875541@qq.com>
2026-06-04 23:51:40 +0800

    完成首页页面基础结构和请求

---
+++ api/http.js
@@ -0,0 +1,41 @@
+
+
+let baseUrl = ''
+
+if(process.env.NODE_ENV === 'development'){
+	//baseUrl = 'http://localhost:5173/api'
+	baseUrl = 'https://m1.apifoxmock.com/m1/4728220-0-default/api/'
+}else{
+	baseUrl = 'https://m1.apifoxmock.com/m1/4728220-0-default/api/'
+}
+
+export default function http (url,data,method){
+	return new Promise((resolve,reject)=>{
+		uni.request({
+			url: baseUrl + url,
+			data,
+			method,
+			header:{
+				'token':uni.getStorage('token') || ''
+			},
+			success: res=>{
+				if(res.statusCode === 200){
+					if(res.data.code === 1){
+						resolve(res.data.data)
+					}else{
+						uni.showToast({
+							title:res.data.msg?res.data.msg:'请求出错',
+							icon:'none'
+						})
+					}
+				}
+			},
+			fail: ()=>{
+				uni.showToast({
+					title:'请求异常',
+					icon:'none'
+				})
+			}
+		})
+	})
+}
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,131 @@
 <template>
-	111111
-	<up-button text="月落"></up-button>
+	<view class="content">
+		<up-search placeholder="搜索🔍" bg-color="#e3e3e3" v-model="keyword"></up-search>
+		<up-swiper
+			v-if="bannerList.length"
+			:list="bannerList"
+			keyName="image"
+			showTitle
+			radius="8"
+		    @change="change"
+		    @click="click"
+			class="banner"
+		></up-swiper>
+		<up-notice-bar text="text1"></up-notice-bar>
+		<view class="list">
+			<up-waterfall v-model="flowList" columns="2">
+				<template v-slot:left="{leftList}">
+					<view class="demo-warter" v-for="(item,index) in leftList" :key="index">
+						<up-lazy-load threshold="-450" border-radius="10" :image="item.img" :index="index"></up-lazy-load>
+						<view class="demo-title">
+							{{item.title}}
+						</view>
+						<view class="demo-price">
+							{{item.times}}
+						</view>
+						<view class="demo-tag">
+							<view class="demo-tag-owner">
+								{{item.tag[0]}}
+							</view>
+							<view class="demo-tag-text">
+								{{item.tag[1]}}
+							</view>
+						</view>
+					</view>
+				</template>
+				<template v-slot:right="{rightList}">
+					<view class="demo-warter" v-for="(item,index) in rightList" :key="index">
+						<up-lazy-load threshold="-450" border-radius="10" :image="item.img" :index="index"></up-lazy-load>
+						<view class="demo-title">
+							{{item.title}}
+						</view>
+						<view class="demo-price">
+							{{item.times}}
+						</view>
+						<view class="demo-tag">
+							<view class="demo-tag-owner">
+								{{item.tag[0]}}
+							</view>
+							<view class="demo-tag-text">
+								{{item.tag[1]}}
+							</view>
+						</view>
+					</view>
+				</template>
+			</up-waterfall>
+		</view>
+	</view>
 </template>
 
 <script setup>
+	import { getBanner,getHomeList } from '../../api/api.js';
+	import { onLoad } from '@dcloudio/uni-app'
+	import { ref,reactive } from 'vue';  
+	  
+	const keyword = ref('');  
+	  
+	const bannerList = ref([]);  
+	const flowList = ref([])
+	
+	onLoad(()=>{
+		getBanner().then(res =>{
+			bannerList.value = res.bannerList
+			console.log(bannerList.value)
+		})
+		getHomeList().then(res=>{
+			console.log(res)
+			flowList.value = res
+		})
+	})
 </script>
 
-<style>
-
+<style lang="scss">
+.content{
+	padding: 20rpx 20rpx;
+}
+.banner{
+	margin: 16rpx 0;
+}
+.list{
+	margin: 30rpx 0;
+	.demo-warter{
+		margin: 10rpx 10rpx 10rpx 0;
+		background-color: #fff;
+		border-radius: 16rpx;
+		padding: 16rpx;
+	}
+	.demo-title{
+		font-size: 30rpx;
+		margin-top: 10rpx;
+		color: #303133;
+	}
+	.demo-price{
+		font-size: 24rpx;
+		color: #777;
+		margin-top: 10rpx;
+	}
+	.demo-tag{
+		display: flex;
+		margin-top: 10rpx;
+		.demo-tag-owner{
+			border: 1px solid rgb(252,163,129);
+			color: #ffaa00;
+			font-size: 20rpx;
+			display: flex;
+			align-items: center;
+			padding: 2rpx 14rpx;
+			border-radius: 50rpx;
+		}
+		.demo-tag-text{
+			border: 1px solid #00aaff;
+			color: #00aaff;
+			font-size: 20rpx;
+			margin-left: 20rpx;
+			display: flex;
+			align-items: center;
+			padding: 2rpx 14rpx;
+			border-radius: 50rpx;
+		}
+	}
+}
 </style>
